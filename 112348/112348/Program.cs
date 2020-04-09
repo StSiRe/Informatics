55ef92c1dd7bc1febe7b03ad435de29afc184eef
Hooli Dev <andrei-asdc2@mail.ru>
2020-04-09 12:46:03 +0300

    Added 112348

---
+++ 112348/112348/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _112348
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp = Console.ReadLine();
+            string tmp1 = Console.ReadLine();
+            string tmp2 = Console.ReadLine();
+            Console.WriteLine(tmp.Replace(tmp1, tmp2));
+        }
+    }
+}
