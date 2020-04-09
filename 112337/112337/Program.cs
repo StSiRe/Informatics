320f65e40aa23d038c8134c56d6a62c87ba2e31a
StSiRe <andrei-asdc2@mail.ru>
2020-04-09 12:34:19 +0300

    Added 112337

---
+++ 112337/112337/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _112337
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp = Console.ReadLine();
+            int counter = 0;
+            foreach(var c in tmp)
+            {
+                if (c == 'a' || c == 'A')
+                    counter++;
+            }
+            tmp = tmp.Replace('a', 'b');
+            tmp = tmp.Replace('A', 'B');
+            Console.WriteLine(tmp);
+            Console.WriteLine(counter);
+        }
+    }
+}
