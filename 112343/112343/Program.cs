ee72edba3413534155509d1562d2ef6f11a4ee50
Hooli Dev <andrei-asdc2@mail.ru>
2020-04-09 12:38:26 +0300

    Added 112343

---
+++ 112343/112343/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _112343
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp, tmp1 = Console.ReadLine();
+            tmp = new string(tmp1.Reverse().ToArray());
+            if (tmp == tmp1)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+            }
+    }
+}
