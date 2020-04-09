8c51a97f2ff95ba15ee2c304f5f29c4334151e26
Hooli Dev <andrei-asdc2@mail.ru>
2020-04-09 12:38:01 +0300

    Added 112340

---
+++ 112340/112340/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _112340
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp = Console.ReadLine();
+            var data = tmp.Split(' ');
+            for (int i = data.Length-1; i >= 0; i--)
+            {
+                if(data[i] != "")
+                {
+                    Console.WriteLine(data[i]);
+                    return;
+                }
+            }
+        }
+    }
+}
