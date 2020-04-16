c2eb912ed0cf150d7cf2869041f79cac15377712
Hooli Dev <andrei-asdc2@mail.ru>
2020-04-16 11:07:01 +0300

    Added 112415

---
+++ 112415/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _112415
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tmp = Console.ReadLine();
+            string tmp1 = "";
+            long res = 0;
+            tmp = "+" + tmp;
+            for (int i = tmp.Length - 1; i >= 0 ; i--)
+            {
+                if (tmp[i] == '+')
+                {
+                    tmp1 = new string(tmp1.Reverse().ToArray());
+                    if (tmp1 != "")
+                        res += Convert.ToInt64(tmp1);
+                    tmp1 = "";
+                }
+                else if(tmp[i]== '-')
+                {
+                    tmp1 = new string(tmp1.Reverse().ToArray());
+                    if (tmp1 != "")
+                        res -= Convert.ToInt64(tmp1);
+                    tmp1 = "";
+                }
+                else
+                {
+                    tmp1 += tmp[i];
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
